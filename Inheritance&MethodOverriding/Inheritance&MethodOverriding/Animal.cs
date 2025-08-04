66879a1b5fa7b154b9c9b98746115407b9651d02
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-08-04 15:40:24 +0000

    Add Inheritance and MethodOverriding project

---
+++ Inheritance&MethodOverriding/Inheritance&MethodOverriding/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_MethodOverriding
+{
+    public  class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound.");
+        }
+    }
+
+    public  class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public  class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+}
---
+++ Inheritance&MethodOverriding/Program.cs
@@ -0,0 +1,15 @@
+﻿using Inheritance_MethodOverriding;
+
+Console.WriteLine("----------------------------");
+Console.WriteLine("--Inheritance & Method Overriding--");
+
+// Animal Instance
+Animal animal = new Animal();
+animal.MakeSound();
+
+// Dog and Cat Instances
+Dog dog = new Dog();
+dog.MakeSound();
+
+Cat cat = new Cat();
+cat.MakeSound();   
