c66ccf1f5268355d9825d463fa48f4c0adc7b6b9
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-08-04 16:19:55 +0000

    Add Abstract classes and methods task

---
+++ AbstractClassesAndMethods/AbstractClassesAndMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AbstractClassesAndMethods;
+
+Console.WriteLine("----------------------------");
+Console.WriteLine("----Abstract Classes And Methods----");
+
+// Computes the area of the circle
+Circle circle = new Circle(5);
+Console.WriteLine($"The are of the circle is: {circle.GetArea():F2}");
+
+// get the area of the rectangle
+Rectangle rectangle = new Rectangle(4, 5);
+Console.WriteLine($"The area of the rectangle is: {rectangle.GetArea():F2}");
---
+++ AbstractClassesAndMethods/AbstractClassesAndMethods/Shape.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAndMethods
+{
+    abstract class Shape
+    {
+        public abstract float GetArea();
+
+    }
+
+     class Circle : Shape
+    {
+        public double Radius 
+        { 
+            get; 
+            set; 
+        }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public override float GetArea()
+        {
+            return (float)(Math.PI * Radius * Radius);
+        }
+    }
+
+     class Rectangle : Shape
+    {
+        public double Width 
+        { 
+            get; 
+            set; 
+        }
+        public double Height 
+        { 
+            get; 
+            set; 
+        }
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public override float GetArea()
+        {
+            return (float)(Width * Height);
+        }
+    }
+}
