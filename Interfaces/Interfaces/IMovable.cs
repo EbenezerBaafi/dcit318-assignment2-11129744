6def7b7045cb73a76ce5a833c7c6345a89c393f3
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-08-04 16:45:57 +0000

    Add Interfaces task

---
+++ Interfaces/Interfaces/IMovable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IMovable
+    {
+        public void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("The car is moving.");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("The bicycle is moving.");
+        }
+    }
+}
---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Interfaces;
+
+Console.WriteLine("-------------------------");
+Console.WriteLine("------Interfaces---------");
+
+Car car = new Car();
+car.Move();
+
+Bicycle bicycle = new Bicycle();
+bicycle.Move();
